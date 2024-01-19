00c09208a8d4223407e5a4e6beb65899388b53f3
Zhang Tianyang <burning9699@gmail.com>
2024-01-19 18:08:28 +0800

    shim: bugfix of `update` and `stat` cpu and memory
    Signed-off-by: Zhang Tianyang <burning9699@gmail.com>

--- crates/shim/src/cgroup.rs
+++ crates/shim/src/cgroup.rs
@@ -19,11 +19,11 @@
 use std::{fs, io::Read, path::Path};
 
 use cgroups_rs::{
-    cgroup::get_cgroups_relative_paths_by_pid, cpuacct::CpuAcctController, hierarchies, Cgroup,
+    cgroup::get_cgroups_relative_paths_by_pid, hierarchies, Cgroup,
     CgroupPid, MaxValue, Subsystem,
 };
 use containerd_shim_protos::{
-    cgroups::metrics::{CPUStat, CPUUsage, MemoryEntry, MemoryStat, Metrics},
+    cgroups::metrics::*,
     protobuf::{well_known_types::any::Any, Message},
     shim::oci::Options,
 };
@@ -111,9 +111,6 @@ pub fn collect_metrics(pid: u32) -> Result<Metrics> {
         Cgroup::load_with_relative_paths(hierarchies::auto(), Path::new("."), path)
     };
 
-    let has_cpuacct: Option<&CpuAcctController> = cgroup.controller_of();
-    let has_cpuacct = has_cpuacct.is_some();
-
     // to make it easy, fill the necessary metrics only.
     for sub_system in Cgroup::subsystems(&cgroup) {
         match sub_system {
@@ -121,29 +118,99 @@ pub fn collect_metrics(pid: u32) -> Result<Metrics> {
                 // not necessary now
             }
             Subsystem::Cpu(cpu_ctr) => {
-                if has_cpuacct {
-                    continue;
-                }
-                let mut cpu_stat = CPUStat::new();
-                let mut cpu_usage = CPUUsage::new();
-                set_cpu_usage(&mut cpu_usage, &cpu_ctr.cpu().stat);
-                cpu_stat.set_usage(cpu_usage);
+                let mut cpu_stat = metrics.cpu.take().unwrap_or_default();
+                set_cpu_usage_and_throttle(&cpu_ctr.cpu().stat, &mut cpu_stat);
                 metrics.set_cpu(cpu_stat);
             }
             Subsystem::CpuAcct(cpuacct_ctr) => {
+                let acct = cpuacct_ctr.cpuacct();
                 let mut cpu_usage = CPUUsage::new();
-                cpu_usage.set_total(cpuacct_ctr.cpuacct().usage);
-                let mut cpu_stat = CPUStat::new();
+                cpu_usage.set_total(acct.usage);
+                cpu_usage.set_kernel(acct.usage_sys);
+                cpu_usage.set_user(acct.usage_user);
+                cpu_usage.set_per_cpu(
+                    acct.usage_percpu
+                        .split_whitespace()
+                        .map(|s| s.parse::<u64>().unwrap_or_default())
+                        .collect(),
+                );
+                let mut cpu_stat = metrics.cpu.take().unwrap_or_default();
                 cpu_stat.set_usage(cpu_usage);
                 metrics.set_cpu(cpu_stat);
             }
             Subsystem::Mem(mem_ctr) => {
+                let mut mem_stat = MemoryStat::new();
+
+                // set memory
                 let mem = mem_ctr.memory_stat();
                 let mut mem_entry = MemoryEntry::new();
                 mem_entry.set_usage(mem.usage_in_bytes);
-                let mut mem_stat = MemoryStat::new();
+                mem_entry.set_limit(mem.limit_in_bytes as u64);
+                mem_entry.set_max(mem.max_usage_in_bytes);
+                mem_entry.set_failcnt(mem.fail_cnt);
                 mem_stat.set_usage(mem_entry);
+
+                // set swap memory
+                let memswap = mem_ctr.memswap();
+                let mut memswap_entry = MemoryEntry::new();
+                memswap_entry.set_usage(memswap.usage_in_bytes);
+                memswap_entry.set_limit(memswap.limit_in_bytes as u64);
+                memswap_entry.set_max(memswap.max_usage_in_bytes);
+                memswap_entry.set_failcnt(memswap.fail_cnt);
+                mem_stat.set_swap(memswap_entry);
+
+                // set kernel memory
+                let kmem = mem_ctr.kmem_stat();
+                let mut kmem_entry = MemoryEntry::new();
+                kmem_entry.set_usage(kmem.usage_in_bytes);
+                kmem_entry.set_limit(kmem.limit_in_bytes as u64);
+                kmem_entry.set_max(kmem.max_usage_in_bytes);
+                kmem_entry.set_failcnt(kmem.fail_cnt);
+                mem_stat.set_kernel(kmem_entry);
+
+                // set tcp memory
+                let kmem_tcp = mem_ctr.kmem_tcp_stat();
+                let mut kmem_tcp_entry = MemoryEntry::new();
+                kmem_tcp_entry.set_usage(kmem_tcp.usage_in_bytes);
+                kmem_tcp_entry.set_limit(kmem_tcp.limit_in_bytes as u64);
+                kmem_tcp_entry.set_max(kmem_tcp.max_usage_in_bytes);
+                kmem_tcp_entry.set_failcnt(kmem_tcp.fail_cnt);
+                mem_stat.set_kernel_tcp(kmem_tcp_entry);
+
+                // all other stats
+                mem_stat.set_active_anon(mem.stat.active_anon);
+                mem_stat.set_active_file(mem.stat.active_file);
+                mem_stat.set_cache(mem.stat.cache);
+                mem_stat.set_dirty(mem.stat.dirty);
+                mem_stat
+                    .set_hierarchical_memory_limit(mem.stat.hierarchical_memory_limit as u64);
+                mem_stat.set_hierarchical_swap_limit(mem.stat.hierarchical_memsw_limit as u64);
+                mem_stat.set_mapped_file(mem.stat.mapped_file);
+                mem_stat.set_pg_fault(mem.stat.pgfault);
+                mem_stat.set_pg_maj_fault(mem.stat.pgmajfault);
+                mem_stat.set_pg_pg_in(mem.stat.pgpgin);
+                mem_stat.set_pg_pg_out(mem.stat.pgpgout);
+                mem_stat.set_rss(mem.stat.rss);
+                mem_stat.set_rss_huge(mem.stat.rss_huge);
+                mem_stat.set_total_active_anon(mem.stat.total_active_anon);
+                mem_stat.set_total_active_file(mem.stat.total_active_file);
+                mem_stat.set_inactive_anon(mem.stat.inactive_anon);
+                mem_stat.set_inactive_file(mem.stat.inactive_file);
+                mem_stat.set_total_cache(mem.stat.total_cache);
+                mem_stat.set_total_dirty(mem.stat.total_dirty);
+                mem_stat.set_total_inactive_anon(mem.stat.total_inactive_anon);
                 mem_stat.set_total_inactive_file(mem.stat.total_inactive_file);
+                mem_stat.set_total_mapped_file(mem.stat.total_mapped_file);
+                mem_stat.set_total_pg_fault(mem.stat.total_pgfault);
+                mem_stat.set_total_pg_maj_fault(mem.stat.total_pgmajfault);
+                mem_stat.set_total_pg_pg_in(mem.stat.total_pgpgin);
+                mem_stat.set_total_pg_pg_out(mem.stat.total_pgpgout);
+                mem_stat.set_total_rss(mem.stat.total_rss);
+                mem_stat.set_total_rss_huge(mem.stat.total_rss_huge);
+                mem_stat.set_total_unevictable(mem.stat.total_unevictable);
+                mem_stat.set_total_writeback(mem.stat.total_writeback);
+                mem_stat.set_unevictable(mem.stat.unevictable);
+                mem_stat.set_writeback(mem.stat.writeback);
                 metrics.set_memory(mem_stat);
             }
             _ => {}
@@ -152,27 +219,32 @@ pub fn collect_metrics(pid: u32) -> Result<Metrics> {
     Ok(metrics)
 }
 
-fn set_cpu_usage(cpu_usage: &mut CPUUsage, stat: &String) {
-    let lines: Vec<&str> = stat.lines().collect();
-
-    for line in lines {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-
-        if parts.len() != 2 {
-            continue;
-        }
-
-        match parts[0] {
-            "usage_usec" => {
-                cpu_usage.set_total(parts[1].parse().unwrap_or_default());
+fn set_cpu_usage_and_throttle(stat: &String, cpu_stat: &mut CPUStat) {
+    for line in stat.lines() {
+        let parts = line.split_whitespace().collect::<Vec<&str>>();
+        if parts.len() == 2 {
+            let value = parts[1].parse::<u64>().unwrap_or_default();
+            match parts[0] {
+                "usage_usec" => {
+                    cpu_stat.mut_usage().set_total(value);
+                }
+                "user_usec" => {
+                    cpu_stat.mut_usage().set_user(value);
+                }
+                "system_usec" => {
+                    cpu_stat.mut_usage().set_kernel(value);
+                }
+                "nr_periods" => {
+                    cpu_stat.mut_throttling().set_periods(value);
+                }
+                "nr_throttled" => {
+                    cpu_stat.mut_throttling().set_throttled_periods(value);
+                }
+                "throttled_time" => {
+                    cpu_stat.mut_throttling().set_throttled_time(value);
+                }
+                _ => {}
             }
-            "user_usec" => {
-                cpu_usage.set_user(parts[1].parse().unwrap_or_default());
-            }
-            "system_usec" => {
-                cpu_usage.set_kernel(parts[1].parse().unwrap_or_default());
-            }
-            _ => {}
         }
     }
 }
@@ -205,6 +277,21 @@ pub fn update_resources(pid: u32, resources: &LinuxResources) -> Result<()> {
             }
             Subsystem::Mem(mem_ctr) => {
                 if let Some(memory) = resources.memory() {
+                    //if swap and limit setting have
+                    if let (Some(limit), Some(swap)) = (memory.limit(), memory.swap()) {
+                        //get current memory_limit
+                        let current = mem_ctr.memory_stat().limit_in_bytes;
+                        // if the updated swap value is larger than the current memory limit set the swap changes first
+                        // then set the memory limit as swap must always be larger than the current limit
+                        if current < swap {
+                            mem_ctr
+                                .set_memswap_limit(swap)
+                                .map_err(other_error!(e, "set memsw limit"))?;
+                            mem_ctr
+                                .set_limit(limit)
+                                .map_err(other_error!(e, "set mem limit"))?;
+                        }
+                    }
                     // set memory limit in bytes
                     if let Some(limit) = memory.limit() {
                         mem_ctr
